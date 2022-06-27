c9853e42e379726a9423827a55e95a0fe3dc276b
zhao luneng <1713175349@qq.com>
2022-06-27 15:45:39 +0800

    change setup

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup,find_packages
 
 setup(
-    name='moiregemerate',
+    name='moiregenerate',
     version='0.1',
     author="zhao luneng",
     author_email="1713175349@qq.com",
     description="generate moire pattern",
     license="MIT",
     keywords="moire pattern",
-    package=["moiregemerate"],
+    package=["moiregenrate"],
     scripts=["bin/moiregenerate-cmd"],
     install_requires=["numpy","matplotlib","ase"],
 )
