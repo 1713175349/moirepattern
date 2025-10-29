fe2a76e26fe156a16524210001926d26c07676fd
1713175349 <1713175349@qq.com>
2025-10-29 16:12:47 +0800

    update version tag

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='moiregenerate',
-    version='0.1.0',
+    version='0.2.0',
     author="zhao luneng",
     author_email="1713175349@qq.com",
     description="Generate moiré patterns for 2D material bilayer structures",
